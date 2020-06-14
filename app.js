5fe0dd488845b90a4685bc8d50a50cb67c52e547
Michael Linden <m.linden@yale.edu>
2020-06-13 23:56:37 -0400

    add preliminary fb logic

--- app.js
+++ app.js
@@ -12,11 +12,49 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Hi I am a chatbot'))
 
 app.get('/webhook/', (req, res) => {
+    // TODO: update this with other token (different than other one)
     if (req.query['hub.verify_token'] === 'our_token_to_be_defined') {
         res.send(req.query['hub.challenge'])
     }
     res.send("Wrong token")
 })
+
+// TODO: fill in with fb token when created
+let token = 
+
+
+app.post('/webhook', (req, res) => {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_events[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendText(sender, "Echo of: " + text.substring(0, 100))
+        }
+    }
+    res.sendStatus(200)
+})
+
+sendText = (sender, text) => {
+    let messageData = {text: text}
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token : token},
+        method: "POST",
+        json: {
+            recipient: {id: sender}, 
+            message: messageData
+        }
+    }), (error, response, body) => {
+        if (error) {
+            console.log("error")
+        } else if (response.body.error) {
+            console.log("response body error")
+        }
+    }
+}
+
 
 app.listen(app.get("port"), () => {
     console.log("Running on port")
