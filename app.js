cba8436acb7c92bb9f083ef31878790d3d76630e
Cody Lin <codyjlin@yahoomail.com>
2020-06-20 15:15:01 -0700

    clin: testing if challenge token needed everytime

--- app.js
+++ app.js
@@ -11,19 +11,12 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hi I am a chatbot."));
 
-app.get("/webhook/", (req, res) => {
-  // console.log("------------- get webhook --------------");
-  // console.log("request is ")
-  // console.log(req);
-  // console.log("----------------------------");
-  // console.log(res);
-  // console.log("--------------- end webhook -----------");
-
-  if (req.query["hub.verify_token"] === "cacheRegister112358") {
-    res.send(req.query["hub.challenge"]);
-  }
-  res.send("Wrong token");
-});
+// app.get("/webhook/", (req, res) => {
+//   if (req.query["hub.verify_token"] === "cacheRegister112358") {
+//     res.send(req.query["hub.challenge"]);
+//   }
+//   res.send("Wrong token");
+// });
 
 let token =
   "EAAEAaBJDBFQBALdr7koRDeS7MMcPvLnUZBuwFMuJ0GNx1jb9cCyQyVHUSX1nbJaMjF3uNpZA71TxegrbvhfQwC29ZAKAmRqJlBUYJtNlZAQwkNTWR91ZAtpBzDWCGgQrOOBZAaxye4mjox6lfw2cgBeJmbQGCRs71HlQW9rrpdlet4dFav7O1f7S2MRnQI8BoZD";
